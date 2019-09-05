9a5650b7ce441d0b08d62fe8657565aaca082986
gauravrawat440 <gauravrawat440@gmail.com>
2019-09-05 23:56:48 +0530

    Updates

---
+++ precache-manifest.a7d346b1af8910b283b5170fda2d38f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9c328aed17dbda655a9b030041fac6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b60cfa97ef7fec5f714f",
+    "url": "/monster-rolodex/static/css/main.094f450b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b60cfa97ef7fec5f714f",
+    "url": "/monster-rolodex/static/js/main.2fab7936.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
